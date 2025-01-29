3a86bee4612e7ac2b4bc3bf0354e15e5b0226ebf
Kolla Lakshmi Nandan <lakshminandan110@gmail.com>
2025-01-29 18:34:53 +0530

    airthmetic py

---
+++ Python programs/airthmetic.py
@@ -0,0 +1,8 @@
+# Program to perform basic arithmetic operations
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
