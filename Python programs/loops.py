17f5ac9685b93c9c27e1b85599a0dfbbdc16849c
Kolla Lakshmi Nandan <lakshminandan110@gmail.com>
2025-01-29 18:38:13 +0530

    initial commit

---
+++ Python programs/loops.py
@@ -0,0 +1,9 @@
+# Program to print numbers from 1 to 10 using a loop
+for i in range(1, 11):
+    print(i, end=" ")
+
+print("\nUsing while loop:")
+n = 1
+while n <= 10:
+    print(n, end=" ")
+    n += 1
