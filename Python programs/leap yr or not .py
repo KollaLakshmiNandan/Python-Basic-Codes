898b53f23e9c50c53892d8e55309b199e433e84e
Kolla Lakshmi Nandan <lakshminandan110@gmail.com>
2025-01-30 16:01:00 +0530

    leap yr,not

---
+++ Python programs/leap yr or not .py
@@ -0,0 +1,8 @@
+print("leapyear or not ")
+year=int(input("enter the year :"))
+if year%4==0 or year%100==0:
+    print("it is an leap year")
+elif year%400==0 :
+    print("it is a leap year")
+else:
+    print("it is not a leap year")
